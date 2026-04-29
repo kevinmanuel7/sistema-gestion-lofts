8e08b7d14704618c306d0d1e15fdc2cc54c8dfaf
fbpz <fe.penaz@duocuc.cl>
2026-04-29 19:10:08 -0400

    commit inicial

---
+++ equipamiento/src/main/java/com/equipamiento/equipamiento/EquipamientoApplication.java
@@ -0,0 +1,13 @@
+package com.equipamiento.equipamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipamientoApplication.class, args);
+	}
+
+}
