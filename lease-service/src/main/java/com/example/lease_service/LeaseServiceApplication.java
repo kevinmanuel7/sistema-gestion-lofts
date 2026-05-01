74c228eec224aaa8a2b63ba84a30751e3bc13a6d
Bastian Veas Contreras <bastianveascontreras@MacBook-Air-de-Bastian.local>
2026-05-01 00:19:23 -0400

    Estructura inicial lease-service puerto 8086

---
+++ lease-service/src/main/java/com/example/lease_service/LeaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.lease_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaseServiceApplication.class, args);
+	}
+
+}
