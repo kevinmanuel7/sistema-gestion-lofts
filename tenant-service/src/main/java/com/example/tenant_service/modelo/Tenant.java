82e92bda3c9bee8c9501f7013195b8efea6f1caf
Bastian Veas Contreras <bastianveascontreras@MacBook-Air-de-Bastian.local>
2026-05-02 23:49:48 -0400

    Estructura inicial: Modelo Tenant y JPA Repository

--- tenant-service/src/main/java/com/example/tenant_service/modelo/Tenant.java
+++ tenant-service/src/main/java/com/example/tenant_service/modelo/Tenant.java
@@ -2,16 +2,19 @@ package com.example.tenant_service.modelo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "Tenant")
+@Table(name = "tenants")
 
 public class Tenant {
     
@@ -19,10 +22,20 @@ public class Tenant {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "el nombre es obligatorio")
     private String nombre;
+ 
+    @NotBlank(message = "El apellido es obligatorio")
     private String apellido;
+
+    @NotBlank(message = "El RUT es obligatorio")
+    @Column(unique = true)
     private String rut;
-    private String email;
+
+    @Email(message = "Debe ingresar un formato de email válido")
+    private String email;  
+
     private String telefono;
+
     private LocalDate fechaRegistro;
 }
---
+++ tenant-service/src/main/java/com/example/tenant_service/repository/TenantRepository.java
@@ -0,0 +1,8 @@
+package com.example.tenant_service.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TenantRepository {
+    
+}
